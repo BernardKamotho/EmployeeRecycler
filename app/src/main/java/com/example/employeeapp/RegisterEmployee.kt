2575e0f51171c6116ee19499c39bef51ba1543b2
bernardkamotho <kbenmotho.bk@gmail.com>
2024-11-13 12:39:54 +0300

    fetched data from API

--- app/src/main/java/com/example/employeeapp/RegisterEmployee.kt
+++ app/src/main/java/com/example/employeeapp/RegisterEmployee.kt
@@ -1,10 +1,13 @@
 package com.example.employeeapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONObject
 
 class RegisterEmployee : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,42 @@ class RegisterEmployee : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        } //end insets
+
+        //find all the 5 editTexts and one button by use of their ids
+
+        val id_number = findViewById<EditText>(R.id.id_number)
+        val username = findViewById<EditText>(R.id.username)
+        val others = findViewById(R.id.others)  as EditText
+        val salary = findViewById<EditText>(R.id.salary)
+        val department = findViewById<EditText>(R.id.department)
+        val savebtn = findViewById<Button>(R.id.saveBtn)
+
+        //add a click listener to our button
+        savebtn.setOnClickListener {
+            //create a json object that will hold data gotten from the five edittexts.
+            // The data is already in the variables creared above
+            val body = JSONObject()
+
+            //add idnumber, username, others, salary and department to the json object
+            body.put("id_number", id_number.text.toString())
+            body.put("username", username.text.toString())
+            body.put("others", others.text.toString())
+            body.put("salary", salary.text.toString())
+            body.put("department", department.text.toString())
+
+            //specify the api endpoint for creating an employee
+            val api = "https://kimaniben.pythonanywhere.com/createEmployee"
+
+            //Access the api helper class
+            //Trigger the post function inside of the api helper class.
+            //Provide the data requires by the two parameters
+
+            val helper = ApiHelper(applicationContext)
+
+            //access the post function
+            helper.post(api,body)
         }
+
     }
 }
--- app/src/main/java/com/example/employeeapp/ViewEmployees.kt
+++ app/src/main/java/com/example/employeeapp/ViewEmployees.kt
@@ -1,10 +1,14 @@
 package com.example.employeeapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONArray
 
 class ViewEmployees : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,44 @@ class ViewEmployees : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        } //end insets
+
+        //find the progressbar by use of its id
+        val progress = findViewById<ProgressBar>(R.id.progress)
+
+        //specify the url to get the employees
+        val api = "https://kimaniben.pythonanywhere.com/RetrieveEmployees"
+
+        //access the helper
+        val helper = ApiHelper(applicationContext)
+
+        //inside of the helper class, access the get function
+        helper.get(api, object : ApiHelper.CallBack {
+            //convert the get results into a json array
+            override fun onSuccess(result: String?) {
+                val employeeJsonArray = JSONArray(result.toString())
+
+                //load the results to the textView
+                val empdata = findViewById<TextView>(R.id.empdata)
+
+                //a single employee
+                (0 until employeeJsonArray.length()).forEach {
+                    val employee = employeeJsonArray.getJSONObject(it)
+                    empdata.append("ID Number:  " + employee.get("id_number") + "\n")
+                    empdata.append("Fisrt Name: " + employee.get("username") + "\n")
+                    empdata.append("Last Name:  " + employee.get("others") + "\n")
+                    empdata.append("salary:     " + employee.get("salary") + "\n")
+                    empdata.append("Department: " + employee.get("department") + "\n")
+                    empdata.append("\n \n")
+                }
+                //stop the progressbar after a successfull fetch of data
+                progress.visibility = View.GONE
+
+
+
+
+            }
+
+        })
     }
 }
